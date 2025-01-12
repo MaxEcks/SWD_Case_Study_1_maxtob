2a07299b783259cde2e335bfc73fa472315f8002
MaxEcks <em9298@mci4me.at>
2025-01-12 11:24:14 +0100

    Revert "trying to add the user_interface.py code of Tobias branch and merge it with master branch"
    This reverts commit 9a19a524d1609d6268dd8e7d647ed08c0e68727f.

--- Workspace/user_interface.py
+++ Workspace/user_interface.py
@@ -1,19 +1,76 @@
 import streamlit as st
 from users import User
 from devices import Device
+import time
 
 st.write("# Gerätemanagement")
 
 tab1, tab2 = st.tabs(["Nutzerverwaltung", "Geräteverwaltung"])
 
 with tab1:
-    #Nutzer anlegen
-    email = st.text_input("E-Mail Adresse eingeben")
-    name = st.text_input("Name eingeben")
+    # Untertabs für Nutzerverwaltung:
+    tab1_1, tab1_2, tab1_3 = st.tabs(["Nutzer anlegen", "Nutzer anzeigen", "Nutzer ändern"])
 
-    st.button("Hinzufügen")
-    
-    st.button("Nutzer entfernen")
+    # Nutzer anlegen:
+    with tab1_1:
+        st.write("### Neuen Nutzer anlegen:")
+        name = st.text_input("Name eingeben")
+        email = st.text_input("E-Mail Adresse eingeben")
+
+        if st.button("Hinzufügen"):
+            existing_user = User.find_by_attribute("id", email)
+            
+            if existing_user:
+                st.error(f"Benutzer mit dieser Email-Adresse existiert bereits: {existing_user}")
+                time.sleep(3)
+                st.rerun()
+
+            else:
+                new_user = User(email, name)
+                new_user.store_data()
+                st.success(f"Neuer Benutzer hinzugefügt: {new_user}")
+                time.sleep(3)
+                st.rerun()
+
+    # Nutzer anzeigen:
+    with tab1_2:
+        st.write("### Liste aller Nutzer:")
+        users = User.find_all()
+        for user in users:
+            st.write(f"Name: {user.name}, E-Mail: {user.id}")
+
+        if st.button("Aktualisieren"):
+            st.success("Aktualisieren erfolgreich.")
+            time.sleep(3)
+            st.rerun()
+
+    # Nutzer ändern:
+    with tab1_3:
+        st.write("### Nutzer ändern:")
+
+        users = User.find_all()
+        user_options = {f"{user.name} ({user.id})": user for user in users}
+        selected_user = st.selectbox("Wählen Sie einen Nutzer aus", list(user_options.keys()))
+
+        if selected_user:
+            user_to_edit = user_options[selected_user]
+            new_name = st.text_input("Name ändern", value=user_to_edit.name, key="edit_name")
+            new_email = st.text_input("E-Mail ändern", value=user_to_edit.id, key="edit_email")
+
+            if st.button("Änderungen speichern"):
+                # User Daten ändern:
+                user_to_edit.name = new_name
+                user_to_edit.id = new_email
+                user_to_edit.store_data()
+                st.success(f"Benutzerdaten aktualisiert: {user_to_edit}")
+                time.sleep(3)
+                st.rerun()
+
+            if st.button("Benutzer löschen"):
+                user_to_edit.delete()
+                st.success(f"Benutzer gelöscht.")
+                time.sleep(3)
+                st.rerun()
 
 with tab2:
     #Geräteverwaltung
@@ -47,34 +104,11 @@ with tab2:
                 st.error("Bitte alle Felder ausfüllen.")
 
     with tab2_2:
-        # Bestehende Geräte ändern
-        
-        devices_in_db = Device.find_all()
+        pass #Hier (Gerät ändern) muss man noch den Aufruf eines bestehenden Gerätes implementieren (z.B. selectbox). Kann aber erst nach der Implementierung der
+             #Datenbank gemacht werden
 
-        device_names = [device.device_name for device in devices_in_db]
-        
-        selected_device_name = st.selectbox("Wählen Sie ein Gerät aus", device_names)
-
-        if selected_device_name:
-            selected_device = Device.find_by_attribute("device_name", selected_device_name)
-            
-            if selected_device:
-                st.write(f"Gerät: {selected_device.device_name}")
-                new_device_name = st.text_input("Gerätename", selected_device.device_name)
-                new_managed_by_user_id = st.text_input("Verantwortliche Person", selected_device.managed_by_user_id)
-                new_maintenance_interval = st.number_input("Wartungsintervall in Tage", min_value=1, step=1, value=selected_device.maintenance_interval)
-                new_maintenance_cost = st.number_input("Wartungskosten in Euro", min_value=0.0, step=0.01, value=selected_device.maintenance_cost)
-
-                if st.button("Änderungen speichern"):
-                    if new_device_name and new_managed_by_user_id:
-                        selected_device.device_name = new_device_name
-                        selected_device.managed_by_user_id = new_managed_by_user_id
-                        selected_device.maintenance_interval = new_maintenance_interval
-                        selected_device.maintenance_cost = new_maintenance_cost
-                                     
-                        selected_device.store_data()
-                        st.success("Gerätedaten wurden aktualisiert!")
-                    else: 
-                        st.error("Fülle alle Felder aus!")
-            else:
-                st.error("Gerät nicht gefunden.")
+# Konsolen Debugging:
+users = User.find_all()
+print("All users:")
+for user in users:
+    print(user)
